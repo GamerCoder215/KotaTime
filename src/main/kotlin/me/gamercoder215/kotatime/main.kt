95b19b930f7cae809a6eaaa5babbdd637d9338c0
Gregory Mitchell <54124162+GamerCoder215@users.noreply.github.com>
2024-01-03 20:27:58 -0600

    Update main.kt

--- src/main/kotlin/me/gamercoder215/kotatime/main.kt
+++ src/main/kotlin/me/gamercoder215/kotatime/main.kt
@@ -1,12 +1,16 @@
 package me.gamercoder215.kotatime
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.jetbrains.skia.Image
 
 @Composable
 @Preview
@@ -15,7 +19,11 @@ fun App() {
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        title = NAME,
+        icon = painterResource(ICON_URL),
+        onCloseRequest = ::exitApplication
+    ) {
         App()
     }
 }
