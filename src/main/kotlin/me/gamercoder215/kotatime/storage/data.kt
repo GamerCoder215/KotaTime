e68d1d97fffa1a5514c5c02f61958751b1c2d5e2
Gregory Mitchell <54124162+GamerCoder215@users.noreply.github.com>
2024-01-06 15:51:55 -0600

    Make Data Classes Serializable

--- src/main/kotlin/me/gamercoder215/kotatime/storage/data.kt
+++ src/main/kotlin/me/gamercoder215/kotatime/storage/data.kt
@@ -1,13 +1,24 @@
 package me.gamercoder215.kotatime.storage
 
+import kotlinx.serialization.json.*
 import me.gamercoder215.kotatime.ICON_URL
+import me.gamercoder215.kotatime.util.load
+import me.gamercoder215.kotatime.util.wrapperType
+import java.io.Serial
+import java.io.Serializable
+import java.lang.reflect.Modifier
 import kotlin.properties.Delegates
 
 // WakaTime Data
 
-class Language {
+class Language : Serializable {
     var name = "Unknown"
     var color = "#000000"
+
+    companion object {
+        @Serial
+        private const val serialVersionUID: Long = 1L
+    }
 }
 
 // WakaTime User Data
@@ -32,24 +43,39 @@ object TimeSinceToday {
 
 // WakaTime User Data (Classes)
 
-class Rank {
+class Rank : Serializable {
     var rank = 0
     var page = 0
-    var language = "Unknown"
+    var language: String? = null
+
+    companion object {
+        @Serial
+        private const val serialVersionUID: Long = 1L
+    }
 }
 
-class Machine {
+class Machine : Serializable {
     var id = "00000000-0000-0000-0000-000000000000"
     var name = "Unknown"
+
+    companion object {
+        @Serial
+        private const val serialVersionUID: Long = 1L
+    }
 }
 
-class Project {
+class Project : Serializable {
     var id = "00000000-0000-0000-0000-000000000000"
     var name = "Unknown"
     var created_at = "1970-01-01T00:00:00Z"
     var color = "#000000"
     var has_public_url = false
     var repository: ProjectRepository? = null
+
+    companion object {
+        @Serial
+        private const val serialVersionUID: Long = 1L
+    }
 }
 
 // WakaTime Stats Data
@@ -60,25 +86,40 @@ class Stats(
     val categories: Set<Stat>,
     val languages: Set<Stat>,
     val operating_systems: Set<Stat>
-)
+) : Serializable {
+    companion object {
+        @Serial
+        private const val serialVersionUID: Long = 1L
+    }
+}
 
-class StatsInfo {
+class StatsInfo : Serializable {
     var is_up_to_date = false
     var range = "all_time"
     var daily_average = 0
     var total_seconds = 0.0
     var total_seconds_including_other_language = 0.0
+
+    companion object {
+        @Serial
+        private const val serialVersionUID: Long = 1L
+    }
 }
 
-class Stat {
+class Stat : Serializable {
     var total_seconds = 0.0
     var percent = 100.0
     var name = "Unknown"
+
+    companion object {
+        @Serial
+        private const val serialVersionUID: Long = 1L
+    }
 }
 
 // GitHub
 
-class ProjectRepository {
+class ProjectRepository : Serializable {
     var default_branch = "master"
     var description = ""
     var homepage: String? = null
@@ -88,16 +129,25 @@ class ProjectRepository {
     var star_count = 0
     var fork_count = 0
     var watch_count = 0
+
+    companion object {
+        @Serial
+        private const val serialVersionUID: Long = 1L
+    }
 }
 
-class CommitData {
+class CommitData : Serializable {
     var total = 0
     var branch = "master"
-
     var commits: Set<Commit> = mutableSetOf()
+
+    companion object {
+        @Serial
+        private const val serialVersionUID: Long = 1L
+    }
 }
 
-class Commit {
+class Commit : Serializable {
     var author_avatar_url = ICON_URL
     var author_date = "1970-01-01T00:00:00Z"
     var author_name = "Unknown"
@@ -112,6 +162,11 @@ class Commit {
     var message = ""
     var ref = "refs/heads/master"
     var total_seconds = 0
+
+    companion object {
+        @Serial
+        private const val serialVersionUID: Long = 1L
+    }
 
     override fun equals(other: Any?): Boolean {
         if (other !is Commit) return false
