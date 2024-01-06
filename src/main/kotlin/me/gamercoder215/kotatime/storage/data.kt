601b5fb4f4702f038944232e696cbcc548150b65
Gregory Mitchell <54124162+GamerCoder215@users.noreply.github.com>
2024-01-05 22:16:17 -0600

    Draft Data Classes

--- src/main/kotlin/me/gamercoder215/kotatime/storage/data.kt
+++ src/main/kotlin/me/gamercoder215/kotatime/storage/data.kt
@@ -1,37 +1,119 @@
 package me.gamercoder215.kotatime.storage
 
-interface Reloadable {
+import kotlin.properties.Delegates
 
-    val fileName: String
+// WakaTime Data
 
-    fun reload(): Reloadable
-
+class Language {
+    var name by Delegates.notNull<String>()
+    var color by Delegates.notNull<String>()
 }
 
 // WakaTime User Data
 
-data class WUser(
-    val bio: String,
-    val created_at: String,
-    val email: String,
-    val github_username: String,
-    val weekday_start: Int,
-    val profile_url: String,
-    val photo: String,
+object WUser {
+    var bio by Delegates.notNull<String>()
+    var created_at by Delegates.notNull<String>()
+    var modified_at by Delegates.notNull<String>()
+    var email by Delegates.notNull<String>()
+    var github_username by Delegates.notNull<String>()
+    var profile_url by Delegates.notNull<String>()
+    var photo by Delegates.notNull<String>()
+    var has_premium_features by Delegates.notNull<Boolean>()
+    var weekday_start by Delegates.notNull<Int>()
+}
+
+object TimeSinceToday {
+    var decimal by Delegates.notNull<String>()
+    var is_up_to_date by Delegates.notNull<Boolean>()
+}
+
+object Rank {
+    var rank by Delegates.notNull<Int>()
+    var page by Delegates.notNull<Int>()
+    var language by Delegates.notNull<String>()
+}
+
+// WakaTime User Data (Classes)
+
+class Machine {
+    var id by Delegates.notNull<String>()
+    var name by Delegates.notNull<String>()
+}
+
+class Project {
+    var id by Delegates.notNull<String>()
+    var name by Delegates.notNull<String>()
+    var created_at by Delegates.notNull<String>()
+    var color by Delegates.notNull<String>()
+    var has_public_url by Delegates.notNull<Boolean>()
+    var repository: ProjectRepository? = null
+}
+
+// WakaTime Stats Data
+
+class Stats(
+    val info: StatsInfo,
+    val categories: List<Stat>,
+    val languages: List<Stat>,
+    val operating_systems: List<Stat>
 )
 
-data class TimeSinceToday(
-    val decimal: String,
-    val startDate: String,
-    val upToDate: Boolean
-)
+class StatsInfo {
+    var is_up_to_date by Delegates.notNull<Boolean>()
+    var range by Delegates.notNull<String>()
+    var daily_average by Delegates.notNull<Int>()
+    var total_seconds by Delegates.notNull<Double>()
+    var total_seconds_including_other_language by Delegates.notNull<Double>()
+}
 
-data class Duration(
-    val projectName: String,
-    val timestamp: Long,
-    val seconds: Double
-)
+class Stat {
+    var total_seconds by Delegates.notNull<Double>()
+    var percent by Delegates.notNull<Double>()
+    var name by Delegates.notNull<String>()
+}
 
 // GitHub
 
-// TODO GitHub Data
+class ProjectRepository {
+    var default_branch by Delegates.notNull<String>()
+    var description by Delegates.notNull<String>()
+    var homepage by Delegates.notNull<String>()
+    var html_url by Delegates.notNull<String>()
+    var image_icon_url by Delegates.notNull<String>()
+    var provider by Delegates.notNull<String>()
+    var star_count by Delegates.notNull<Int>()
+    var fork_count by Delegates.notNull<Int>()
+    var watch_count by Delegates.notNull<Int>()
+}
+
+class CommitData {
+    var total by Delegates.notNull<Int>()
+    var branch by Delegates.notNull<String>()
+
+    var commits: Set<Commit> = mutableSetOf()
+}
+
+class Commit {
+    var author_avatar_url by Delegates.notNull<String>()
+    var author_date by Delegates.notNull<String>()
+    var author_name by Delegates.notNull<String>()
+    var author_username by Delegates.notNull<String>()
+    var author_email by Delegates.notNull<String>()
+    var branch by Delegates.notNull<String>()
+    var truncated_hash by Delegates.notNull<String>()
+    var hash by Delegates.notNull<String>()
+    var html_url by Delegates.notNull<String>()
+    var human_readable_date by Delegates.notNull<String>()
+    var human_readable_natural_date by Delegates.notNull<String>()
+    var message by Delegates.notNull<String>()
+    var ref by Delegates.notNull<String>()
+    var total_seconds by Delegates.notNull<Int>()
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is Commit) return false
+        return hash == other.hash
+    }
+
+    override fun hashCode(): Int = hash.hashCode()
+}
