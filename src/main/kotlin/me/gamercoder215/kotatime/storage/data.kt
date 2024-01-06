59aec3a5ba8a32598604163c75c66cb46285aa4c
Gregory Mitchell <54124162+GamerCoder215@users.noreply.github.com>
2024-01-06 02:01:56 -0600

    Add Default Data Values

--- src/main/kotlin/me/gamercoder215/kotatime/storage/data.kt
+++ src/main/kotlin/me/gamercoder215/kotatime/storage/data.kt
@@ -1,52 +1,54 @@
 package me.gamercoder215.kotatime.storage
 
+import me.gamercoder215.kotatime.ICON_URL
 import kotlin.properties.Delegates
 
 // WakaTime Data
 
 class Language {
-    var name by Delegates.notNull<String>()
-    var color by Delegates.notNull<String>()
+    var name = "Unknown"
+    var color = "#000000"
 }
 
 // WakaTime User Data
 
 object WUser {
-    var bio by Delegates.notNull<String>()
-    var created_at by Delegates.notNull<String>()
-    var modified_at by Delegates.notNull<String>()
-    var email by Delegates.notNull<String>()
-    var github_username by Delegates.notNull<String>()
-    var profile_url by Delegates.notNull<String>()
-    var photo by Delegates.notNull<String>()
-    var has_premium_features by Delegates.notNull<Boolean>()
-    var weekday_start by Delegates.notNull<Int>()
+    var bio = ""
+    var created_at = "1970-01-01T00:00:00Z"
+    var modified_at = "1970-01-01T00:00:00Z"
+    var email: String? = null
+    var github_username: String? = null
+    var profile_url = "https://wakatime.com"
+    var photo = ICON_URL
+    var has_premium_features = false
+    var twitter_username: String? = null
+    var weekday_start = 0
 }
 
 object TimeSinceToday {
-    var decimal by Delegates.notNull<String>()
-    var is_up_to_date by Delegates.notNull<Boolean>()
-}
-
-object Rank {
-    var rank by Delegates.notNull<Int>()
-    var page by Delegates.notNull<Int>()
-    var language by Delegates.notNull<String>()
+    var decimal = "0.0"
+    var is_up_to_date = true
 }
 
 // WakaTime User Data (Classes)
 
+class Rank {
+    var rank = 0
+    var page = 0
+    var language = "Unknown"
+}
+
 class Machine {
-    var id by Delegates.notNull<String>()
-    var name by Delegates.notNull<String>()
+    var id = "00000000-0000-0000-0000-000000000000"
+    var name = "Unknown"
 }
 
 class Project {
-    var id by Delegates.notNull<String>()
-    var name by Delegates.notNull<String>()
-    var created_at by Delegates.notNull<String>()
-    var color by Delegates.notNull<String>()
-    var has_public_url by Delegates.notNull<Boolean>()
+    var id = "00000000-0000-0000-0000-000000000000"
+    var name = "Unknown"
+    var created_at = "1970-01-01T00:00:00Z"
+    var color = "#000000"
+    var has_public_url = false
     var repository: ProjectRepository? = null
 }
 
@@ -54,61 +56,62 @@ class Project {
 
 class Stats(
     val info: StatsInfo,
-    val categories: List<Stat>,
-    val languages: List<Stat>,
-    val operating_systems: List<Stat>
+    val editors: Set<Stat>,
+    val categories: Set<Stat>,
+    val languages: Set<Stat>,
+    val operating_systems: Set<Stat>
 )
 
 class StatsInfo {
-    var is_up_to_date by Delegates.notNull<Boolean>()
-    var range by Delegates.notNull<String>()
-    var daily_average by Delegates.notNull<Int>()
-    var total_seconds by Delegates.notNull<Double>()
-    var total_seconds_including_other_language by Delegates.notNull<Double>()
+    var is_up_to_date = false
+    var range = "all_time"
+    var daily_average = 0
+    var total_seconds = 0.0
+    var total_seconds_including_other_language = 0.0
 }
 
 class Stat {
-    var total_seconds by Delegates.notNull<Double>()
-    var percent by Delegates.notNull<Double>()
-    var name by Delegates.notNull<String>()
+    var total_seconds = 0.0
+    var percent = 100.0
+    var name = "Unknown"
 }
 
 // GitHub
 
 class ProjectRepository {
-    var default_branch by Delegates.notNull<String>()
-    var description by Delegates.notNull<String>()
-    var homepage by Delegates.notNull<String>()
-    var html_url by Delegates.notNull<String>()
-    var image_icon_url by Delegates.notNull<String>()
-    var provider by Delegates.notNull<String>()
-    var star_count by Delegates.notNull<Int>()
-    var fork_count by Delegates.notNull<Int>()
-    var watch_count by Delegates.notNull<Int>()
+    var default_branch = "master"
+    var description = ""
+    var homepage: String? = null
+    var html_url = "https://github.com"
+    var image_icon_url: String? = null
+    var provider = "github"
+    var star_count = 0
+    var fork_count = 0
+    var watch_count = 0
 }
 
 class CommitData {
-    var total by Delegates.notNull<Int>()
-    var branch by Delegates.notNull<String>()
+    var total = 0
+    var branch = "master"
 
     var commits: Set<Commit> = mutableSetOf()
 }
 
 class Commit {
-    var author_avatar_url by Delegates.notNull<String>()
-    var author_date by Delegates.notNull<String>()
-    var author_name by Delegates.notNull<String>()
-    var author_username by Delegates.notNull<String>()
-    var author_email by Delegates.notNull<String>()
-    var branch by Delegates.notNull<String>()
-    var truncated_hash by Delegates.notNull<String>()
-    var hash by Delegates.notNull<String>()
-    var html_url by Delegates.notNull<String>()
-    var human_readable_date by Delegates.notNull<String>()
-    var human_readable_natural_date by Delegates.notNull<String>()
-    var message by Delegates.notNull<String>()
-    var ref by Delegates.notNull<String>()
-    var total_seconds by Delegates.notNull<Int>()
+    var author_avatar_url = ICON_URL
+    var author_date = "1970-01-01T00:00:00Z"
+    var author_name = "Unknown"
+    var author_username = "Unknown"
+    var author_email = ""
+    var branch = "master"
+    var truncated_hash = "0000000"
+    var hash = "0000000000000000000000000000000000000000"
+    var html_url = "https://github.com"
+    var human_readable_date = "Jan 1, 1970"
+    var human_readable_natural_date = "0 seconds ago"
+    var message = ""
+    var ref = "refs/heads/master"
+    var total_seconds = 0
 
     override fun equals(other: Any?): Boolean {
         if (other !is Commit) return false
