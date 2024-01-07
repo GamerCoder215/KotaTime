a52872f83e900788f173b4bc4102e05191fed103
Gregory Mitchell <54124162+GamerCoder215@users.noreply.github.com>
2024-01-06 22:01:25 -0600

    Finish Serialization

--- src/main/kotlin/me/gamercoder215/kotatime/storage/StorageManager.kt
+++ src/main/kotlin/me/gamercoder215/kotatime/storage/StorageManager.kt
@@ -1,6 +1,7 @@
 package me.gamercoder215.kotatime.storage
 
 import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.JsonObject
 import me.gamercoder215.kotatime.ICON_URL
 import me.gamercoder215.kotatime.json
 import me.gamercoder215.kotatime.util.*
@@ -10,6 +11,8 @@ object StorageManager {
 
     val folder = File("${System.getProperty("user.home")}${File.separator}.kotatime")
 
+    val settings: MutableMap<String, String> = mutableMapOf()
+
     val languages: MutableSet<Language> = mutableSetOf()
 
     val machines: MutableSet<Machine> = mutableSetOf()
@@ -18,12 +21,23 @@ object StorageManager {
     val ranks: MutableSet<Rank> = mutableSetOf()
     val commits: MutableMap<String, CommitData> = mutableMapOf()
 
+    val isGlobalEmpty: Boolean
+        get() {
+            val langs = File(folder, "languages.dat")
+            return !langs.exists()
+        }
+
     val isEmpty: Boolean
-        get() =
-            WUser.email == null && machines.isEmpty() && projects.isEmpty() && loadedStats.isEmpty() && ranks.isEmpty() && commits.isEmpty()
+        get() {
+            val user = File(folder, "user")
+            return !user.exists() || user.list()?.isEmpty() ?: true
+        }
 
     fun clear() {
+        settings.clear()
+
         languages.clear()
+
         machines.clear()
         projects.clear()
         loadedStats.clear()
@@ -34,21 +48,47 @@ object StorageManager {
         TimeSinceToday.clearFields()
     }
 
+    // Saving
+
+    fun saveSettings() {
+        // Settings
+        val settings = File(folder, "settings.json")
+        if (!settings.exists()) settings.createNewFile()
+        settings.writeText(json.encodeToString(this.settings))
+    }
+
+    fun saveGlobalData() {
+        if (languages.isNotEmpty()) {
+            // Languages
+            val langs = File(folder, "languages.dat")
+            if (!langs.exists()) langs.createNewFile()
+            langs.serialize(languages)
+        }
+    }
+
+    fun savePhoto() {
+        val user = File(folder, "user")
+        if (!user.exists()) user.mkdirs()
+
+        val picture = File(user, "profile.png")
+        if (!picture.exists()) {
+            picture.createNewFile()
+            picture.writeBytes(WUser.photo.asData)
+        }
+
+        WUser.photo = picture.absolutePath
+    }
+
     fun save() {
         if (!folder.exists()) folder.mkdirs()
-
-        // Languages
-        val langs = File(folder, "languages.dat")
-        if (!langs.exists()) langs.createNewFile()
-        langs.serialize(languages)
+        saveSettings()
+        saveGlobalData()
 
         // User
         val user = File(folder, "user")
         if (!user.exists()) user.mkdirs()
 
-        val picture = File(user, "profile.png")
-        if (!picture.exists()) picture.createNewFile()
-        picture.writeBytes(WUser.photo.asData)
+        savePhoto()
 
         val userData = File(user, "data.json")
         if (!userData.exists()) userData.createNewFile()
@@ -59,17 +99,17 @@ object StorageManager {
         timeSinceToday.writeText(json.encodeToString(TimeSinceToday.toJson()))
 
         // Machines
-        val machs = File(folder, "machines.dat")
+        val machs = File(user, "machines.dat")
         if (!machs.exists()) machs.createNewFile()
         machs.serialize(machines)
 
         // Ranks
-        val rank = File(folder, "ranks.dat")
+        val rank = File(user, "ranks.dat")
         if (!rank.exists()) rank.createNewFile()
         rank.serialize(ranks)
 
         // Projects
-        val projects = File(folder, "projects")
+        val projects = File(user, "projects")
         if (!projects.exists()) projects.mkdirs()
 
         for (proj in this.projects) {
@@ -97,13 +137,28 @@ object StorageManager {
         }
     }
 
+    // Loading
+
+    fun loadSettings() {
+        // Settings
+        val settings = File(folder, "settings.json")
+        if (!settings.exists()) settings.createNewFile()
+        this.settings.putAll(json.decodeFromString(settings.readText()))
+    }
+
+    fun loadGlobalData() {
+        // Languages
+        if (languages.isEmpty()) {
+            val langs = File(folder, "languages.dat")
+            if (!langs.exists()) langs.createNewFile()
+            languages.addAll(langs.deserialize<Set<Language>>() ?: emptySet())
+        }
+    }
+
     fun load() {
         if (!folder.exists()) folder.mkdirs()
-
-        // Languages
-        val langs = File(folder, "languages.dat")
-        if (!langs.exists()) langs.createNewFile()
-        languages.addAll(langs.deserialize<Set<Language>>() ?: emptySet())
+        loadSettings()
+        loadGlobalData()
 
         // User
         val user = File(folder, "user")
