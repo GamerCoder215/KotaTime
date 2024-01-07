14ea0371b38460a42c4b931c79afad3fb698d2b6
Gregory Mitchell <54124162+GamerCoder215@users.noreply.github.com>
2024-01-06 22:00:28 -0600

    Draft UI Constants

---
+++ src/main/kotlin/me/gamercoder215/kotatime/ui/constants.kt
@@ -0,0 +1,9 @@
+package me.gamercoder215.kotatime.ui
+
+import me.gamercoder215.kotatime.storage.StorageManager.saveSettings
+
+// Colors
+
+const val DARK_BACKGROUND = 0xFF_222222
+
+const val LIGHT_BACKGROUND = 0xFF_F0F0F0
