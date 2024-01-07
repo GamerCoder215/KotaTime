837a78c838c4da36c44c5c015b519831fdcc4a63
Gregory Mitchell <54124162+GamerCoder215@users.noreply.github.com>
2024-01-06 22:01:37 -0600

    Update UI & General Utilities

--- src/main/kotlin/me/gamercoder215/kotatime/ui/util.kt
+++ src/main/kotlin/me/gamercoder215/kotatime/ui/util.kt
@@ -4,9 +4,30 @@ import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.FrameWindowScope
 import io.kanro.compose.jetbrains.expui.control.ProgressBar
+import io.kanro.compose.jetbrains.expui.control.ToolBarActionButton
+import io.kanro.compose.jetbrains.expui.theme.DarkTheme
+import org.jetbrains.skia.Image
+import java.io.File
+
+@Composable
+fun smallSpacer() = Spacer(Modifier.size(14.dp))
+
+@Composable
+fun mediumSpacer() = Spacer(Modifier.size(28.dp))
+
+@Composable
+fun largeSpacer() = Spacer(Modifier.size(54.dp))
+
+// Functions
 
 @Composable
 fun InfiniteProgressBar() {
@@ -17,3 +38,13 @@ fun InfiniteProgressBar() {
 
     ProgressBar(currentOffset)
 }
+
+// Extensions
+
+val String.asImage: ImageBitmap
+    get() {
+        val stream = File(this).inputStream()
+        return Image.makeFromEncoded(stream.readBytes()).toComposeImageBitmap()
+    }
+
+fun Modifier.background(value: Long, shape: Shape = RectangleShape) = background(Color(value), shape)
--- src/main/kotlin/me/gamercoder215/kotatime/util/util.kt
+++ src/main/kotlin/me/gamercoder215/kotatime/util/util.kt
@@ -1,19 +1,18 @@
 package me.gamercoder215.kotatime.util
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kotlinx.serialization.json.*
+import java.io.*
 import java.lang.reflect.Field
+import java.net.URL
 import java.time.Instant
 import java.time.format.DateTimeFormatter
-import java.util.Date
-
-@Composable
-fun smallSpacer() = Spacer(Modifier.size(14.dp))
-
-@Composable
-fun mediumSpacer() = Spacer(Modifier.size(28.dp))
+import java.util.*
+import java.lang.reflect.Modifier as ReflectionModifier
 
 // Extensions
 
@@ -39,3 +38,85 @@ val String.asDate: Date
     get() = Date.from(Instant.from(
         DateTimeFormatter.ISO_INSTANT.parse(this)
     ))
+
+fun <T> T.load(json: JsonObject): T {
+    for (field in this!!::class.java.declaredFields)
+        field.load(this, json[field.name] as? JsonPrimitive ?: continue)
+
+    return this
+}
+
+fun Field.load(instance: Any?, value: JsonElement) {
+    if (value !is JsonPrimitive) return
+
+    isAccessible = true
+
+    when (wrapperType) {
+        Int::class.java -> this[instance] = value.intOrNull ?: return
+        Double::class.java -> this[instance] = value.doubleOrNull ?: return
+        String::class.java -> this[instance] = value.content
+        Boolean::class.java -> this[instance] = value.booleanOrNull ?: return
+    }
+}
+
+fun <T> File.serialize(obj: T) {
+    val oos = ObjectOutputStream(BufferedOutputStream(outputStream()))
+    oos.writeObject(obj)
+    oos.close()
+}
+
+@Suppress("unchecked_cast")
+fun <T> File.deserialize(): T? {
+    val ois = ObjectInputStream(BufferedInputStream(inputStream()))
+    val obj = ois.readObject()
+    ois.close()
+    return obj as? T
+}
+
+val String.asData: ByteArray
+    get() {
+        return if (startsWith("https://"))
+            URL(this).readBytes()
+        else asFileData
+    }
+
+val String.asFileData: ByteArray
+    get() = File(this).readBytes()
+
+fun Any.toJson(): JsonObject = buildJsonObject {
+    for (field in this::class.java.declaredFields.filter { !ReflectionModifier.isFinal(it.modifiers) }.onEach { it.isAccessible = true }) {
+        val value = field[null]
+        if (value == null) {
+            put(field.name, JsonNull)
+            continue
+        }
+
+        when (field.wrapperType) {
+            String::class.java -> put(field.name, value as? String)
+            Int::class.java -> put(field.name, value as Int)
+            Double::class.java -> put(field.name, value as Double)
+            Boolean::class.java -> put(field.name, value as Boolean)
+            else -> throw IllegalStateException("Unknown type: '${field.wrapperType}' for field '${field.name}'")
+        }
+    }
+}
+
+fun Any.fromJson(obj: JsonObject) {
+    for (field in this::class.java.declaredFields.filter { !ReflectionModifier.isFinal(it.modifiers) }) {
+        val value = obj[field.name] ?: continue
+        if (value is JsonNull) continue
+
+        field.load(this, value)
+    }
+}
+
+fun Any.clearFields() {
+    for (field in this::class.java.declaredFields.filter { !ReflectionModifier.isFinal(it.modifiers) }.onEach { it.isAccessible = true })
+        field[this] = when (field.wrapperType) {
+            String::class.java -> ""
+            Int::class.java -> 0
+            Double::class.java -> 0.0
+            Boolean::class.java -> false
+            else -> throw IllegalStateException("Unknown type: '${field.wrapperType}' for field '${field.name}'")
+        }
+}
